88c906dd4666d26a1c93689c1239bfee2151d3f0
PYU224 <muikamachi8993@protonmail.com>
2025-07-29 00:58:20 +0900

    bugfix

---
+++ client-scripts/video-watch-client-plugin.d.ts
@@ -0,0 +1,4 @@
+import { RegisterClientOptions } from '@peertube/peertube-types/client';
+declare function register({ registerHook, peertubeHelpers }: RegisterClientOptions): Promise<void>;
+export { register };
+//# sourceMappingURL=video-watch-client-plugin.d.ts.map
